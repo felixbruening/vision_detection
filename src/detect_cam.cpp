d95653c83687d510a1f93579e3fe7588e22b8063
Felix Brüning <fbrning@uni-bremen.de>
2018-04-03 18:13:20 +0200

    new

--- src/detect_cam.cpp
+++ src/detect_cam.cpp
@@ -3,9 +3,23 @@
 //
 
 #include "detect_cam.h"
-#include "opencv/cv.h"
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 
+using namespace cv;
+using namespace std;
 
 int main(){
 
+    VideoCapture camera;
+
+    Mat frame;
+
+    capture.open(0);
+
+    while(1){
+        camera.read(frame);
+
+        imgshow("Test", frame);
+    }
 }
