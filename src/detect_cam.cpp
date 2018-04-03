38b7dfb3d6fc6ac3c080813064f18045df558433
Felix Brüning <fbrning@uni-bremen.de>
2018-04-03 18:02:22 +0200

    added c++ files

---
+++ src/detect_cam.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Felix Brüning on 03.04.18.
+//
+
+#include "detect_cam.h"
+#include "opencv/cv.h"
+
+
+int main(){
+
+}
---
+++ src/detect_cam.h
@@ -0,0 +1,14 @@
+//
+// Created by Felix Brüning on 03.04.18.
+//
+
+#ifndef PROJECT_DETECT_CAM_H
+#define PROJECT_DETECT_CAM_H
+
+
+class detect_cam {
+
+};
+
+
+#endif //PROJECT_DETECT_CAM_H
